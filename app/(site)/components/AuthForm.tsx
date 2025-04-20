df530950d7f419cc9182bfea4c9cbf36337f47a5
Gerald <geraldbaalham@live.co.uk>
2025-04-20 12:00:54 +0100

    Add user profile image to the project

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,123 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import Button from "@/app/components/Button";
+import Input from "./inputs/Input";
+import { FcGoogle } from "react-icons/fc";
+import { BsGithub } from "react-icons/bs";
+type Variant = "LOGIN" | "REGISTER";
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "LOGIN") {
+      //login
+    }
+    if (variant === "REGISTER") {
+      //register
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div className="mt-8  sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button fullWidth disabled={isLoading} type="submit">
+              {variant === "LOGIN" ? "Sign In" : "Register"}
+            </Button>
+          </div>
+        </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-8 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+            <div className="relative flex justify-center text-sm mt-5">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2 justify-center">
+            <AuthSocialButton
+              icon={FcGoogle}
+              onClick={() => socialAction("google")}
+            />
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-2 justify-center text-sm mt-6 text-gray-500">
+        <div>
+          {variant === "LOGIN"
+            ? "New to Neural Feed Messenger?"
+            : "Already have an account?"}
+        </div>
+        <div
+          onClick={toggleVariant}
+          className="underline text-indigo-500 hover:text-indigo-600 cursor-pointer"
+        >
+          {variant === "LOGIN" ? "Create An Account" : "Login"}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AuthForm;
